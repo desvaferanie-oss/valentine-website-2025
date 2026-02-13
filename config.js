d77bb0a7946ac54ea1f547f5ee8952b07062beb8
desvaferanie-oss <desvaferanie@gmail.com>
2026-02-13 20:28:10 +0700

    Update config.js

--- config.js
+++ config.js
@@ -50,8 +50,8 @@ const CONFIG = {
     // Messages that appear after they say "Yes!"
     celebration: {
         title: "Yay! I'm so lucky to have you in my life 💗",
-        message: "Now come get your gift, a lot of kiss 💋💋",
-        emojis: "🎁💖🤗💝💋❤️💕"  // These will bounce around
+        message: "Now come get your gift, a lot of kiss",
+        emojis: "💋💋💋💋💋"  // These will bounce around
     },
 
     // Color scheme for the website
